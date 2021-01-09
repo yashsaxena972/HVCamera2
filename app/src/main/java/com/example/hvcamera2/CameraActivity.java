e9f221984ab24ad0bfa091b48a10d8f3daa83ce4
Yash Saxena <yash972saxena@gmail.com>
2021-01-09 14:29:54 +0530

    Added Camera1 capabilities - 1.0

--- app/src/main/java/com/example/hvcamera2/CameraActivity.java
+++ app/src/main/java/com/example/hvcamera2/CameraActivity.java
@@ -2,13 +2,33 @@ package com.example.hvcamera2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Camera.PictureCallback mPicture;
+    private Context myContext;
+    private LinearLayout previewImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myContext = this;
+
+        mCamera =  Camera.open();
+        mCamera.setDisplayOrientation(90);
+        previewImageView = (LinearLayout) findViewById(R.id.preview_image_view);
+        mPreview = new CameraPreview(myContext, mCamera);
+        previewImageView.addView(mPreview);
     }
 }
