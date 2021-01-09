f727627ff49f743b67c9b4399aa84ba732938a9c
Yash Saxena <yash972saxena@gmail.com>
2021-01-09 13:55:06 +0530

    Skeleton 0.0

---
+++ app/src/main/java/com/example/hvcamera2/Camera2Activity.java
@@ -0,0 +1,14 @@
+package com.example.hvcamera2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Camera2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
